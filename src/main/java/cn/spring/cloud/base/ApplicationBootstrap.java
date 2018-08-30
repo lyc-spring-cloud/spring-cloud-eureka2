29c0971edb3195f2d456aaaac9c578b1bbade891
刘永超 <liuyongchao@mofanghr.com>
2018-08-30 14:00:37 +0800

    init eureka

---
+++ src/main/java/cn/spring/cloud/base/ApplicationBootstrap.java
@@ -0,0 +1,12 @@
+package cn.spring.cloud.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBootstrap.class, args);
+	}
+}
